7e081505692aec2e2702ced012328731c0ea9812
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2024-02-28 16:36:44 -0300

    desafio useState

--- 03-React-hooks/hooks/01-useState/src/App.jsx
+++ 03-React-hooks/hooks/01-useState/src/App.jsx
@@ -1,20 +1,41 @@
 import React from 'react';
+import Produto from './Produto';
+
+// Os links abaixo puxam dados de um produto em formato JSON
+// https://ranekapi.origamid.dev/json/api/produto/tablet
+// https://ranekapi.origamid.dev/json/api/produto/smartphone
+// https://ranekapi.origamid.dev/json/api/produto/notebook
+// Crie uma interface com 3 botões, um para cada produto.
+// Ao clicar no botão faça um fetch a api e mostre os dados do produto na tela.
+// Mostre apenas um produto por vez
+// Mostre a mensagem carregando... enquanto o fetch é realizado
 
 const App = () => {
-  const [ativo, setAtivo] = React.useState(false);
-  const [dados, setDados] = React.useState({ nome: 'Joao lucas', idade: '26' });
-
-  function handleClick() {
-    setAtivo(!ativo);
-    setDados({ ...dados, faculdade: 'Possui faculdade' });
+  const [dados, setDados] = React.useState(null);
+  const [carregando, setCarregando] = React.useState(null);
+  async function handleClick(event) {
+    setCarregando(true);
+    const response = await fetch(
+      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`,
+    );
+    const json = await response.json();
+    console.log(json);
+    setDados(json);
+    setCarregando(false);
   }
-
   return (
     <div>
-      <p>{dados.nome}</p>
-      <p>{dados.idade}</p>
-      <p>{dados.faculdade}</p>
-      <button onClick={handleClick}>{ativo ? 'ativo' : 'Inativo'}</button>
+      <button style={{ margin: '.5rem' }} onClick={handleClick}>
+        notebook
+      </button>
+      <button style={{ margin: '.5rem' }} onClick={handleClick}>
+        smartphone
+      </button>
+      <button style={{ margin: '.5rem' }} onClick={handleClick}>
+        tablet
+      </button>
+      {carregando && <p>Carregando</p>}
+      {!carregando && dados && <Produto dados={dados} />}
     </div>
   );
 };
