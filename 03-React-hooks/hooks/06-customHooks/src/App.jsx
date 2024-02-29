24e85263121642ae9c02a2c1fd9a34ab44a6afae
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2024-02-29 11:24:34 -0300

    custom hook

--- 03-React-hooks/hooks/06-customHooks/src/App.jsx
+++ 03-React-hooks/hooks/06-customHooks/src/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import useLocalStorage from './useLocalStorage';
 
 const App = () => {
-  return <div>App React</div>;
+  const [produto, setProduto] = useLocalStorage('produto', '');
+
+  function handleClick({ target }) {
+    setProduto(target.innerText);
+  }
+
+  return (
+    <div>
+      <p>Preferido: {produto}</p>
+      <button onClick={handleClick}>notebook</button>
+      <button onClick={handleClick}>smartphone</button>
+    </div>
+  );
 };
 
 export default App;
