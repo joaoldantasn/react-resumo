7fc203dbdeb6e2236aed285a3c675eeac7831d3a
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2024-03-01 18:47:29 -0300

    forms-input

---
+++ 04-Formularios/formularios/01-input/src/App.jsx
@@ -0,0 +1,116 @@
+import React from 'react';
+
+// Faça um fetch (POST) para a API abaixo
+// Para a criação ser aceita é necessário enviar dodos de:
+// nome, email, senha, cep, rua, numero, bairro, cidade e estado
+
+// Essa é a função utilizado para realizar o POST
+//fetch('https://ranekapi.origamid.dev/json/api/usuario', {
+//method: 'POST',
+//headers: {
+//'Content-Type': 'application/json',
+//},
+// form é o objeto com os dados do formulário
+//body: JSON.stringify(form),
+//});
+
+// Mostre uma mensagem na tela, caso a resposta da API seja positiva
+
+const formFields = [
+  {
+    id: 'nome',
+    label: 'Nome',
+    type: 'text',
+  },
+  {
+    id: 'email',
+    label: 'Email',
+    type: 'email',
+  },
+  {
+    id: 'senha',
+    label: 'Senha',
+    type: 'password',
+  },
+  {
+    id: 'cep',
+    label: 'Cep',
+    type: 'text',
+  },
+  {
+    id: 'rua',
+    label: 'Rua',
+    type: 'text',
+  },
+  {
+    id: 'numero',
+    label: 'Numero',
+    type: 'text',
+  },
+  {
+    id: 'bairro',
+    label: 'Bairro',
+    type: 'text',
+  },
+  {
+    id: 'cidade',
+    label: 'Cidade',
+    type: 'text',
+  },
+  {
+    id: 'estado',
+    label: 'Estado',
+    type: 'text',
+  },
+];
+
+const App = () => {
+  const [form, setForm] = React.useState({
+    nome: '',
+    email: '',
+    senha: '',
+    cep: '',
+    rua: '',
+    numero: '',
+    bairro: '',
+    cidade: '',
+    estado: '',
+  });
+
+  const [response, setResponse] = React.useState(null);
+
+  function handleChange({ target }) {
+    const { id, value } = target;
+    setForm({ ...form, [id]: value });
+    console.log(form);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetch('https://ranekapi.origamid.dev/json/api/usuario', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      // form é o objeto com os dados do formulário
+      body: JSON.stringify(form),
+    }).then((response) => {
+      setResponse(response);
+    });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {formFields.map(({ id, label, type }) => (
+        <div key={id}>
+          <label htmlFor={id}>{label}</label>
+          <input type={type} id={id} value={form[id]} onChange={handleChange} />
+        </div>
+      ))}
+      {response && response.ok && <p>Formulário Enviado</p>}
+      <button>Enviar</button>
+    </form>
+  );
+};
+
+export default App;
