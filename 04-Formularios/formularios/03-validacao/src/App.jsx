b112f2d6153e4b82c7ff35738b8f813bdeeffc73
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2024-03-04 16:23:53 -0300

    validate

---
+++ 04-Formularios/formularios/03-validacao/src/App.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import Input from './Form/Input';
+
+const App = () => {
+  const [cep, setCep] = React.useState('');
+  const [error, setError] = React.useState('');
+
+  function validateCep(value) {
+    if (value.length === 0) {
+      setError('Preencha um valor');
+      return false;
+    } else if (!/^\d{5}-?\d{3}$/.test(value)) {
+      setError('Preencha um CEP válido');
+      return false;
+    } else {
+      setError(null);
+      return true;
+    }
+  }
+
+  function handleBlur({ target }) {
+    validateCep(target.value);
+  }
+
+  function handleChange({ target }) {
+    if (error) validateCep(target.value);
+    setCep(target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (validateCep(cep)) {
+      console.log('Enviou');
+    } else {
+      console.log('Fumo');
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Input
+        label="CEP"
+        id="cep"
+        type="text"
+        value={cep}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        placeholder="00000-000"
+      />
+      {error && <p>{error}</p>}
+      <button>Enviar</button>
+    </form>
+  );
+};
+
+export default App;
