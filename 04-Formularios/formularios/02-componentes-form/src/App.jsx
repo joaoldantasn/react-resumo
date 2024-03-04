cabf4cd969ce7c79fb7077478904c4a397658627
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2024-03-04 14:55:08 -0300

    Checkbox

--- 04-Formularios/formularios/02-componentes-form/src/App.jsx
+++ 04-Formularios/formularios/02-componentes-form/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Checkbox from './Form/Checkbox';
 import Input from './Form/Input';
 import Radio from './Form/Radio';
 import Select from './Form/Select';
@@ -11,6 +12,9 @@ const App = () => {
   const [produto, setProduto] = React.useState('');
   //radio
   const [cor, setCor] = React.useState('');
+  const [frutas, setFrutas] = React.useState('');
+  //checkbox
+  const [linguagens, setLinguagens] = React.useState([]);
 
   return (
     <div>
@@ -28,7 +32,21 @@ const App = () => {
         setValue={setProduto}
       />
       <br></br>
+      <h2>Cores</h2>
       <Radio options={['Azul', 'Vermelho']} value={cor} setValue={setCor} />
+      <h2>Frutas</h2>
+      <Radio
+        options={['Limão', 'Laranja', 'Uva']}
+        value={frutas}
+        setValue={setFrutas}
+      />
+      <br></br>
+      <h2>Linguagens de Programação</h2>
+      <Checkbox
+        options={['JavaScript', 'PHP', 'Ruby']}
+        value={linguagens}
+        setValue={setLinguagens}
+      />
       <br></br>
       <button>Enviar</button>
     </div>
