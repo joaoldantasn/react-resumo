fd6dd19f02daf5191dd09fa58dbbb978542c1106
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2024-03-04 14:43:10 -0300

    Radio

--- 04-Formularios/formularios/02-componentes-form/src/App.jsx
+++ 04-Formularios/formularios/02-componentes-form/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Input from './Form/Input';
+import Radio from './Form/Radio';
 import Select from './Form/Select';
 
 const App = () => {
@@ -8,6 +9,8 @@ const App = () => {
   const [email, setEmail] = React.useState('');
   //select
   const [produto, setProduto] = React.useState('');
+  //radio
+  const [cor, setCor] = React.useState('');
 
   return (
     <div>
@@ -24,6 +27,8 @@ const App = () => {
         value={produto}
         setValue={setProduto}
       />
+      <br></br>
+      <Radio options={['Azul', 'Vermelho']} value={cor} setValue={setCor} />
       <br></br>
       <button>Enviar</button>
     </div>
