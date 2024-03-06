fcd6ea4c96cce436aa4b46b69177bc9c54f47dd8
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2024-03-06 11:00:16 -0300

    useParams

--- 06-Router/02-link/src/App.jsx
+++ 06-Router/02-link/src/App.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Header';
+import Home from './Home';
+import Login from './Login';
+import NotFound from './NotFound';
+import Produto from './Produto';
+import Sobre from './Sobre';
 
 const App = () => {
-  return <div>App React</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="sobre" element={<Sobre />} />
+        <Route path="login" element={<Login />} />
+        <Route path="produto/:id" element={<Produto />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ 06-Router/02-link/src/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <nav>
+      <NavLink to="/" end>
+        Home
+      </NavLink>{' '}
+      | <NavLink to="sobre">Sobre</NavLink>| <NavLink to="login">Login</NavLink>
+    </nav>
+  );
+};
+
+export default Header;
