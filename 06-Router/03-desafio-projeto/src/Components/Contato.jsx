50b6da8bd2e97433684c3a59078eb15d8d489395
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2024-03-08 10:46:58 -0300

    Contato stylized

--- 06-Router/03-desafio-projeto/src/Components/Contato.jsx
+++ 06-Router/03-desafio-projeto/src/Components/Contato.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import styles from './Contato.module.css';
+import foto from '../img/contato.jpg';
 
 const Contato = () => {
   return (
-    <div>
-      <h1>Contato</h1>
-    </div>
+    <section className={`${styles.contato} animeLeft`}>
+      <img src={foto} alt="Máquina de escrever" />
+      <div>
+        <h1>Entre em contato conosco</h1>
+        <ul className={styles.dados}>
+          <li>joaoldantasn@gmail.com</li>
+          <li>(83) 99999-9999</li>
+          <li>Rua da caixa de água</li>
+        </ul>
+      </div>
+    </section>
   );
 };
 
