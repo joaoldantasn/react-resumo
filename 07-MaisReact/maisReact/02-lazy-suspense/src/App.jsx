7287649db773e9f27654976b43f77a7aa7ea91c7
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2024-03-08 17:04:32 -0300

    Memo

--- 07-MaisReact/maisReact/02-lazy-suspense/src/App.jsx
+++ 07-MaisReact/maisReact/02-lazy-suspense/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import Header from './Header';
 //import Contato from './Contato';
 
 const Contato = React.lazy(() => import('./Contato'));
 
 const App = () => {
+  const [contar, setContar] = React.useState(0);
   return (
     <div>
+      <Header />
+      <button onClick={() => setContar(contar + 1)}>{contar}</button>
       <React.Suspense fallback={<div>Carregando...</div>}>
         <Contato />
       </React.Suspense>
