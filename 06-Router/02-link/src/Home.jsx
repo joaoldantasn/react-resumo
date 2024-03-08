840a6f7b9b74343538757159e9fe9778fe8973c5
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2024-03-08 09:42:30 -0300

    helmet

--- 06-Router/02-link/src/Home.jsx
+++ 06-Router/02-link/src/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Head from './Head';
+import { Helmet } from 'react-helmet';
 
 const Home = () => {
   return (
     <div>
-      <Head title="Home" description="Essa é a descrição da Home" />
+      {/*<Head title="Home" description="Essa é a descrição da Home" /> */}
+      <Helmet>
+        <title>Link | Home</title>
+        <meta name="description" content="Essa é a descrição da Home" />
+      </Helmet>
       <h1>Home</h1>
       <p>Essa é a home.</p>
       <Link to="produto/notebook">Notebook</Link>{' '}
