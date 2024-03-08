e89fc4b7ba8ab01253b563ec7b949d76d8a87a53
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2024-03-08 16:05:59 -0300

    Finish

--- 06-Router/03-desafio-projeto/src/Components/Produto.jsx
+++ 06-Router/03-desafio-projeto/src/Components/Produto.jsx
@@ -25,7 +25,7 @@ const Produto = () => {
     fetchProduto(`https://ranekapi.origamid.dev/json/api/produto/${id}`);
   }, [id]);
 
-  if (loading) return <div>Carregando</div>;
+  if (loading) return <div className="loading"></div>;
   if (error) return <p>{error}</p>;
   if (produto === null) return null;
   return (
@@ -34,9 +34,11 @@ const Produto = () => {
         title={`Ranek | ${produto.nome}`}
         description={`Esse é o ${produto.nome}`}
       />
-      {produto.fotos.map((foto) => (
-        <img key={foto.src} src={foto.src} alt={foto.titulo} />
-      ))}
+      <div>
+        {produto.fotos.map((foto) => (
+          <img key={foto.src} src={foto.src} alt={foto.titulo} />
+        ))}
+      </div>
       <div>
         <h1>{produto.nome}</h1>
         <span className={styles.preco}>R$ {produto.preco}</span>
