2de5ba67a93783453362742fae2bb7ccfb7732ff
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2024-03-11 14:48:22 -0300

    forms components

--- 08-ProjetoFinal/projeto/src/Pages/Login/LoginForm.jsx
+++ 08-ProjetoFinal/projeto/src/Pages/Login/LoginForm.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+//componentes
+import Input from '../../Components/Forms/Input';
+import Button from '../../Components/Forms/Button';
+
 const LoginForm = () => {
   const [username, setUsername] = React.useState('');
   const [password, setPassword] = React.useState('');
@@ -31,21 +35,9 @@ const LoginForm = () => {
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={username}
-          onChange={({ target }) => {
-            setUsername(target.value);
-          }}
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={({ target }) => {
-            setPassword(target.value);
-          }}
-        />
-        <button>Entrar</button>
+        <Input label="Usuário" type="text" name="username" />
+        <Input label="Senha" type="password" name="password" />
+        <Button>Entrar</Button>
       </form>
       <Link to="/login/criar">Cadastros</Link>
     </section>
