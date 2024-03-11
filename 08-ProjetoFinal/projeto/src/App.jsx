2e59c252ac56bcb8b619ab8dad71f6517d072fef
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2024-03-11 09:58:30 -0300

    Header

--- 08-ProjetoFinal/projeto/src/App.jsx
+++ 08-ProjetoFinal/projeto/src/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import Api from './api/Api';
 import './App.css';
+//import Api from './api/Api';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+//Components
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+//Pages
+import Home from './Pages/Home';
+import Login from './Pages/Login/Login';
 
 const App = () => {
   return (
     <div>
-      <Api />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 };
