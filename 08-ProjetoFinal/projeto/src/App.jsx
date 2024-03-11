7a592eaead8bc5a9a23c93c236cad86a73c0b6d3
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2024-03-11 20:20:53 -0300

    user context

--- 08-ProjetoFinal/projeto/src/App.jsx
+++ 08-ProjetoFinal/projeto/src/App.jsx
@@ -8,17 +8,21 @@ import Footer from './Components/Footer';
 //Pages
 import Home from './Pages/Home';
 import Login from './Pages/Login/Login';
+//userContext
+import { UserStorage } from './UserContext';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="login/*" element={<Login />} />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
