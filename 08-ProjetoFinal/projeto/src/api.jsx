6f3013a6f175b24608fc96a0d79f46b011a7fce5
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2024-03-21 11:15:28 -0300

    Password lost/reset

--- 08-ProjetoFinal/projeto/src/api.jsx
+++ 08-ProjetoFinal/projeto/src/api.jsx
@@ -55,6 +55,31 @@ export function USER_POST(body) {
   };
 }
 
+export function PASSWORD_LOST(body) {
+  return {
+    url: API_URL + '/api/password/lost',
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    },
+  };
+}
+export function PASSWORD_RESET(body) {
+  return {
+    url: API_URL + '/api/password/reset',
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    },
+  };
+}
+
 //** PHOTOS ENDPOINTS */
 
 //PHOTOPOST
